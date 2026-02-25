4d67f2f92f9bccae470866be287a2cd42d9dc030
chauhanrajendar410-art <chauhanrajendar410@gmail.com>
2026-02-25 19:03:13 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,27 @@
+const billionaires = [
+  {
+    "name": "Elon Musk",
+    "country": "USA",
+    "cars": ["Tesla Model S", "BMW M5"],
+    "family": 10,
+    "food": "Non-Veg",
+    "meat": "Beef, Chicken",
+    "best_work": "Donated billions to science & space research",
+    "networth": "$210 Billion",
+    "photo": "elon.jpg",
+    "video": "https://youtube.com/embed/xyz"
+  },
+
+  {
+    "name": "Gautam Adani",
+    "country": "India",
+    "cars": ["Rolls Royce Ghost", "BMW 7 Series"],
+    "family": 4,
+    "food": "Veg",
+    "meat": "None",
+    "best_work": "Education & Healthcare donations",
+    "networth": "$65 Billion",
+    "photo": "adani.jpg",
+    "video": "https://youtube.com/embed/abc"
+  }
+];
